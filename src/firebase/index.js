2b99ace25a6a4beb1a33f801496c69f18c0b0035
Marcos J. Alvarez <garusis@gmail.com>
2017-04-17 22:49:54 -0500

    Add basic firebase setup

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,11 +1,12 @@
+'use strict'
 /**
  * Created by garusis on 17/04/17.
  */
-
 import * as firebase from "firebase"
+import defaultSettings from "./default_settings"
 
 export let firebaseApp = null
 
-export default function configure (settings = {}) {
-
+export default function configure (settings = defaultSettings) {
+    firebaseApp = firebase.initializeApp(settings)
 }
