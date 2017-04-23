b1a0dd817f3d2e7312f9ca2716eee337c8119d38
Marcos J. Alvarez <garusis@gmail.com>
2017-04-22 21:29:52 -0500

    Add initial reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
 // Set up your root reducer here...
- import { combineReducers } from 'redux';
- export default combineReducers;
+ import { combineReducers } from "redux"
+
+ export default combineReducers({})
---
+++ src/reducers/initialState.js
@@ -0,0 +1,8 @@
+/**
+ * Created by garusis on 22/04/17.
+ */
+export default {
+  courses: [],
+  authors: [],
+  ajaxCallsInProgress: 0
+}
