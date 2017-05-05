40846577f47195ec6e17ddad3b91e7d4cd9e9cb8
Marcos J. Alvarez <garusis@gmail.com>
2017-05-04 21:30:07 -0500

    Add settings

---
+++ src/components/social-media/index.js
@@ -0,0 +1,9 @@
+"use strict"
+/**
+ * Created by garusis on 04/05/17.
+ */
+
+
+export function config (settings) {
+
+}
---
+++ src/components/social-media/settings.js
@@ -0,0 +1,7 @@
+"use strict"
+/**
+ * Created by garusis on 04/05/17.
+ */
+
+export default {
+}
