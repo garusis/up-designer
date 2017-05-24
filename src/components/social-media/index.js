9779d94a6d4cf1eb88e1f9adbdabbee2f1124a85
Marcos J. Alvarez <garusis@gmail.com>
2017-05-24 14:28:44 -0500

    Add config providers function

--- src/components/social-media/index.js
+++ src/components/social-media/index.js
@@ -2,8 +2,12 @@
 /**
  * Created by garusis on 04/05/17.
  */
+import _ from "lodash"
 
+const providersSettings = {}
 
-export function config (settings) {
+export default providersSettings
 
+export function config(provider, settings) {
+  providersSettings[provider] = _.assign(providersSettings[provider] || {}, settings)
 }
--- src/components/social-media/settings.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-/**
- * Created by garusis on 04/05/17.
- */
-
-export default {
-
-}
